961e3971d2731bf3fa2561335573e04c7a74b68f
Abhinav Sukumar Rao <39939017+AetherPrior@users.noreply.github.com>
2021-04-23 01:51:40 +0530

    Remove path hardcoding

--- interpolate.py
+++ interpolate.py
@@ -3,16 +3,18 @@ import numpy as np
 import pandas as pd
 from re import search
 
-def interpolate(parent_dir,output_dir):
-    l = os.listdir(path=parent_dir)
+def interpolate(input_dir,output_dir):
+    l = os.listdir(path=input_dir)
     for f in l:
         if search("csv",f):
-            df = pd.read_csv(os.path.join(parent_dir,f))
+            df = pd.read_csv(os.path.join(input_dir,f))
             df.columns = ['date','lat','long','temp']
             df['temp'] = df['temp'].replace(0,np.nan).interpolate()*0.02-273.16
             df.to_csv(os.path.join(output_dir,f),index=True,columns=['lat','long','temp'])
             
 if __name__ == '__main__':
-    interpolate("./pixel_timeseries/","./interpolated")
+    input_dir = input("Enter input dir of dataset: ")
+    output_dir = input("Enter output dir for interpolated dataset: ")
+    interpolate(input_dir,output_dir)
         
     
